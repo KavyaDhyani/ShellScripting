0a1ced6655c83937dcb93648a0578088b8f2120d
KavyaDhyani <kavyadhyani10@gmail.com>
2026-08-18 06:04:16 +0000

    first commit

---
+++ script1.sh
@@ -0,0 +1,5 @@
+mkdir hello2
+cd hello2
+echo "This is first output" > app.log
+echo "This is second output" >> app.log
+cat app.log
---
+++ varible.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+name="Hello World"
+echo $variable
